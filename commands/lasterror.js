06ff7e249c43d7642d303ef7fc6c69f662e53294
Shooter__Andy <shooter.andy@gmail.com>
2019-06-10 13:27:44 +0700

    Added the !lastError command (admin-only)

---
+++ commands/lasterror.js
@@ -0,0 +1,28 @@
+const _ = require('underscore');
+const fs = require('fs');
+module.exports = (args) => {
+    const message = args.message;
+    const client = args.client;
+    if(message.author.id) {
+        client.fetchApplication().then(application => {
+            if(message.author.id !== application.owner.id) {
+                console.error('-- > User ' + message.author.username + '#' + message.author.tag + '(' +
+                    message.author.id + ') attempted to get the list of servers!');
+                return message.reply('ERROR: You cannot use this command.').catch(console.error);
+            }
+            fs.readFile('./lastError', 'utf8', function (err, data) {
+                if (err) {
+                    return message.reply('Could not read the lastError file.').catch(console.error);
+                }
+                else {
+                    if(data.length) {
+                        return message.reply(data).catch(console.error);
+                    }
+                    else {
+                        return message.reply('lastError file is empty.').catch(console.error);
+                    }
+                }
+            });
+        }).catch(console.error);
+    }
+};
--- events/error.js
+++ events/error.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
 module.exports = (client, error) => {
+    let errorText = '**Error Name:** ' + error.name + '\n**Error Message:**\n```' + error.message + '```';
+    fs.writeFile('./lastError', errorText, function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
     console.error(error);
 };
