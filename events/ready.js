cf78964783b8d2cc5540a82da4c7d412f0bce0af
Shooter__Andy <shooter.andy@gmail.com>
2020-03-22 01:45:06 +0700

    Updating the discord.js package to 12.0.2 to prevent a bug that prevents the bot from logging in.

--- events/ready.js
+++ events/ready.js
@@ -1,26 +1,12 @@
-const fs = require('fs');
-
 module.exports = client => {
     console.log('-- > Logged in as ' + client.user.tag + '!');
 
     const pjson = require('../package.json');
-    client.user.setActivity('v' + pjson.version + ', type !help').catch(console.error);
-
-    /*const weirdChannel = client.channels.find('id', '265156361791209475');
-    if(weirdChannel) {
-        console.log('Weird Channel name: "' + weirdChannel.name + '", guildId: "' + weirdChannel.guild.id +
-            '", guildName:' + weirdChannel.guild.name);
-    }*/
+    client.user.setActivity('v' + pjson.version + ', type !help', { type: '' }).catch(console.error);
 
     const DBL = require('dblapi.js');
     const dbl = new DBL(process.env.DBL_TOKEN, client);
-    /*fs.readdir('./dblevents/', (err, files) => {
-        files.forEach(file => {
-            const eventHandler = require('../dblevents/' + file);
-            const eventName = file.split('.')[0];
-            dbl.webhook.on(eventName, arg => eventHandler(client, arg));
-        });
-    });*/
+
     if(client.shard) {
         console.log('-- > Shard id: "' + client.shard.id + '"; count: "' + client.shard.count + '"');
     }
