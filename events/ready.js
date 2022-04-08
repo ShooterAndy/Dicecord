35edea319d771031f4eda441d6c95e22b87b6314
Shooter__Andy <shooter.andy@gmail.com>
2022-04-08 11:06:55 +0700

    Experimenting with slash commands

--- events/ready.js
+++ events/ready.js
@@ -56,16 +56,19 @@ const deleteExpiredSavedRollCommands = async () => {
   }
 }
 
-const removeWarningInteractivity = async (client, warning) => {
+const removeWarningInteractivity = async (warning) => {
   let channel
   try {
-    channel = await client.channels.fetch(warning[MESSAGES_COLUMNS.channel_id])
+    channel = await Client.getChannelById(warning[MESSAGES_COLUMNS.channel_id])
   } catch (error) {
     if (error && error.name === 'DiscordAPIError') {
       logger.warn(`Failed to fetch channel for a warning`, error)
     } else {
       logger.error(`Failed to fetch channel for a warning`, error)
     }
+    return
+  }
+  if (!channel) {
     return
   }
   let message
@@ -79,7 +82,9 @@ const removeWarningInteractivity = async (client, warning) => {
     }
     return
   }
-
+  if (!message) {
+    return
+  }
   try {
     await message.delete()
   } catch (error) {
@@ -111,7 +116,7 @@ const deleteExpiredWarningMessages = async () => {
         const channelId = warning[MESSAGES_COLUMNS.channel_id]
         const messageId = warning[MESSAGES_COLUMNS.message_id]
         Client.reactionsCache[channelId+ '_' + messageId] = null
-        removeWarningInteractivity(Client.client, warning)
+        removeWarningInteractivity(warning)
       })
     }
   } catch (error) {
@@ -171,7 +176,7 @@ module.exports = async (client) => {
   const DBL = require('dblapi.js')
   const dbl = new DBL(process.env.DBL_TOKEN, client)
 
-  if(client.shard) {
+  if (client.shard) {
     logger.log(`Shard ids: "${client.shard.ids.join('", "')}"; count: "${client.shard.count}"`)
   }
   else {
@@ -190,6 +195,17 @@ module.exports = async (client) => {
       }
     }
   }, transformMinutesToMs(30))
+
+  try {
+    const channel = await Client.getChannelById(process.env.LOG_CHANNEL_ID)
+    if (channel) {
+      const mes = await Client.getMessageByIdAndChannelId(
+        channel.lastMessageId, channel.id)
+      let i = 0
+    }
+  } catch (err) {
+    logger.error(`Failed to get channel`, err)
+  }
 
   await tryToSetActivity()
   setInterval(async () => {
--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const { ShardingManager } = require('discord.js')
 const logger = require('./helpers/logger')
 require('dotenv').config()
 const manager = new ShardingManager('./bot.js', { token: process.env.BOT_TOKEN })
+const nws = require('./helpers/nws')
 
-manager.on('shardCreate', shard => logger.log(`Launched shard ${shard.id}`))
+manager.on('shardCreate', shard => {
+  logger.log(`Launched shard ${shard.id}`)
+
+  shard.on('death', process => {
+    logger.error(nws`Shard ${shard.id} closed unexpectedly! PID: "${process.pid }"; 
+      Exit code: "${process.exitCode}".`)
+
+    if (process.exitCode === null) {
+      logger.warn(nws`WARNING: Shard "${shard.id}" exited with NULL error code. This may be a 
+      result of a lack of available system memory. Ensure that there is enough memory allocated 
+      to continue.`)
+    }
+  })
+})
+
 manager.spawn().then(() => {
   logger.log('Launched sharding manager')
 }).catch(err => {
