c36b2329b7011b825d46ca3b99ed22898a4d07be
Shooter__Andy <shooter.andy@gmail.com>
2022-09-06 22:31:06 +0200

    Making sure the channels the replies are sent to are text channels.

--- helpers/replyOrSendInteraction.js
+++ helpers/replyOrSendInteraction.js
@@ -3,13 +3,18 @@ const logger = require('./logger')
 module.exports = async (interaction, content) => {
   if (interaction) {
     if (interaction.isRepliable() && !interaction.replied) {
-      content.fetchReply = true
-      return await interaction.reply(content)
-        .catch(e => logger.error('Failed to reply to an interaction', e))
+      if (interaction.channel && (typeof interaction.channel.isText === 'function') &&
+        interaction.channel.isText()) {
+        content.fetchReply = true
+        return await interaction.reply(content)
+          .catch(e => logger.error('Failed to reply to an interaction', e))
+      } else {
+        content.fetchReply = true
+        return await interaction.followUp(content)
+          .catch(e => logger.error('Failed to follow up an interaction', e))
+      }
     } else {
-      content.fetchReply = true
-      return await interaction.followUp(content)
-        .catch(e => logger.error('Failed to follow up an interaction', e))
+      logger.error(`Malformed channel in replyOrSendInteraction:\n${JSON.stringify(interaction)}`)
     }
   } else {
      logger.error('No interaction in replyOrSendInteraction')
