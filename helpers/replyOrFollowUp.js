73ab91c2fad93ba1f1f887713a1f64fca6b4e6e9
Shooter__Andy <shooter.andy@gmail.com>
2022-09-08 01:00:47 +0200

    Potentially making replyOrFollowUp a bit more robust.

--- helpers/replyOrFollowUp.js
+++ helpers/replyOrFollowUp.js
@@ -8,24 +8,30 @@ module.exports = async (interaction, content) => {
   if (interaction) {
     if (interaction.channel && (typeof interaction.channel.isText === 'function') &&
       interaction.channel.isText()) {
-      if (interaction.isRepliable() && !interaction.replied) {
-        content.fetchReply = true
-        return await interaction.editReply(content)
-          .catch(e => logger.error(nws`Failed to reply to an interaction in \
+      if (interaction.isRepliable()) {
+        if (!interaction.replied) {
+          content.fetchReply = true
+          return await interaction.editReply(content)
+            .catch(e => logger.error(nws`Failed to reply to an interaction in \
             replyOrFollowUp:\n${JSON.stringify(content)}`, e))
+        } else {
+          content.fetchReply = true
+          return await interaction.followUp(content)
+            .catch(e => logger.error(nws`Failed to follow up an interaction in \
+            replyOrFollowUp:\n${JSON.stringify(content)}`, e))
+        }
       } else {
-        content.fetchReply = true
-        return await interaction.followUp(content)
-          .catch(e => logger.error(nws`Failed to follow up an interaction in \
-            replyOrFollowUp:\n${JSON.stringify(content)}`, e))
+        logger.error(nws`Tried to reply to an interaction that is not repliable in \
+            replyOrFollowUp:\n${JSON.stringify(content)}`)
+        return null
       }
     } else {
       if (interaction.channel) {
-        if (interaction.channel.type === 'GUILD_VOICE') { // This is expected
+        if (interaction.channel.type === 'GUILD_VOICE') { // This is expected, bug in Discord.js
           return null
         } else {
           logger.error(nws`Malformed channel in \
-            replyOrSendInteraction:\n${JSON.stringify(interaction.channel)}`)
+            replyOrFollowUp:\n${JSON.stringify(interaction.channel)}`)
           return null
         }
       } else {
